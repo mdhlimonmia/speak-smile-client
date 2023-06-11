5cf6510b02ce1d10c6034a5253914c269d66e650
Mdhlimon <mdhlimonmia@gmail.com>
2023-06-12 01:58:17 +0600

    new course send databage done

--- src/pages/Dashboard/Instructor/AddCourse/AddCourse.jsx
+++ src/pages/Dashboard/Instructor/AddCourse/AddCourse.jsx
@@ -1,13 +1,41 @@
 import { useContext } from "react";
 import { useForm } from "react-hook-form";
 import { AuthContext } from "../../../../providers/AuthProvider";
+import Swal from "sweetalert2";
 
 const AddCourse = () => {
   const { user } = useContext(AuthContext);
   const { register, handleSubmit, reset } = useForm();
-  const onSubmit = (data) => {};
+  const onSubmit = (data) => {
+    console.log(data);
+
+    fetch("http://localhost:5000/courses", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        if (data.insertedId) {
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: 'Post course successfully.',
+                showConfirmButton: false,
+                timer: 1500
+            });
+        }
+        reset();
+      });
+  };
   return (
     <div className="w-full px-10">
+      <h1 className="text-3xl text-center text-blue-500 font-bold p-5">
+        Add Your New Course
+      </h1>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="form-control w-full mb-4">
           <label className="label">
@@ -38,7 +66,7 @@ const AddCourse = () => {
           <input
             type="text"
             readOnly
-            placeholder={user?.displayName}
+            value={user?.displayName}
             {...register("instructorName", { required: true })}
             className="input input-bordered w-full "
           />
@@ -50,8 +78,8 @@ const AddCourse = () => {
           <input
             type="Email"
             readOnly
-            placeholder={user?.email}
-            {...register("email", { required: true })}
+            value={user?.email}
+            {...register("instructorEmail", { required: true })}
             className="input input-bordered w-full "
           />
         </div>
@@ -61,7 +89,7 @@ const AddCourse = () => {
               <span className="label-text font-semibold">Available seats*</span>
             </label>
             <input
-              type="text"
+              type="number"
               placeholder="Available seats"
               {...register("seat", { required: true })}
               className="input input-bordered w-full "
@@ -72,11 +100,25 @@ const AddCourse = () => {
               <span className="label-text font-semibold">Price*</span>
             </label>
             <input
-              type="text"
+              type="number"
               placeholder="Price"
               {...register("price", { required: true })}
               className="input input-bordered w-full "
             />
+
+            <input
+              type="text"
+              value="pending"
+              {...register("status", { required: true })}
+              className="input input-bordered w-full hidden "
+            />
+            <input
+              type="number"
+              value="0"
+              {...register("studentsEnrolled", { required: true })}
+              className="input input-bordered w-full hidden "
+            />
+
           </div>
         </div>
         <input
